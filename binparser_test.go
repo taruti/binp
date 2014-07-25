46585e08b9281a41ca91d9dc958b1f58a59c8fae
Taru Karttunen <taruti@taruti.net>
2014-07-25 16:25:07 +0300

    Import

---
+++ binparser_test.go
@@ -0,0 +1,23 @@
+package native
+
+import "testing"
+
+type vs struct {
+	v64 uint64
+	v32 uint32
+	v16 uint16
+	v8  byte
+	s   string
+}
+
+func TestParser(t *testing.T) {
+	v := vs{0x8877665544332211, 0x01234567, 0xFEDC, 42, "fofof"}
+	bs := NewPrinter().U64(v.v64).U32(v.v32).U16(v.v16).Byte(v.v8).Align(4).U32String(v.s).Out()
+	var r vs
+
+	NewParser(bs).U64(&r.v64).U32(&r.v32).U16(&r.v16).Byte(&r.v8).Align(4).U32String(&r.s).End()
+
+	if v != r {
+		t.Fatal("Print->Parse->Equal failed for vs")
+	}
+}
