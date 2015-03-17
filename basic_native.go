d5543e261ecb0b5047d0504b2439df210231dad6
Taru Karttunen <taruti@taruti.net>
2015-03-17 20:22:09 +0200

    More refactoring

--- basic_native.go
+++ basic_native.go
@@ -1,12 +1,43 @@
-/*
-The binp package provides parsing and printing of binary values.
-It is particularly suitable for parsing irregular data structures provided by e.g. kernel.
+// build 386, amd64, arm
 
-*/
 package binp
 
-import "encoding/binary"
+var NativeEndian Native
 
-var NativeEndian = func() binary.ByteOrder {
-	return binary.LittleEndian
-}()
+type Native struct{}
+
+func (Native) Uint16(b []byte) uint16 {
+	return uint16(b[0]) | uint16(b[1])<<8
+}
+
+func (Native) PutUint16(b []byte, v uint16) {
+	b[0] = byte(v)
+	b[1] = byte(v >> 8)
+}
+
+func (Native) Uint32(b []byte) uint32 {
+	return uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
+}
+
+func (Native) PutUint32(b []byte, v uint32) {
+	b[0] = byte(v)
+	b[1] = byte(v >> 8)
+	b[2] = byte(v >> 16)
+	b[3] = byte(v >> 24)
+}
+
+func (Native) Uint64(b []byte) uint64 {
+	return uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
+		uint64(b[4])<<32 | uint64(b[5])<<40 | uint64(b[6])<<48 | uint64(b[7])<<56
+}
+
+func (Native) PutUint64(b []byte, v uint64) {
+	b[0] = byte(v)
+	b[1] = byte(v >> 8)
+	b[2] = byte(v >> 16)
+	b[3] = byte(v >> 24)
+	b[4] = byte(v >> 32)
+	b[5] = byte(v >> 40)
+	b[6] = byte(v >> 48)
+	b[7] = byte(v >> 56)
+}
---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+The binp package provides parsing and printing of binary values.
+It is particularly suitable for parsing irregular data structures provided by e.g. kernel.
+
+*/
+package binp
