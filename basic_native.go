983014bd3f708fd4f658a0668ff3b3d6198574c5
Taru Karttunen <taruti@taruti.net>
2016-09-23 10:49:24 +0300

    Eliminate some bounds checks

--- basic_native.go
+++ basic_native.go
@@ -1,4 +1,4 @@
-// build 386, amd64, arm
+// +build 386, arm, amd64
 
 package binp
 
@@ -7,19 +7,23 @@ var NativeEndian Native
 type Native struct{}
 
 func (Native) Uint16(b []byte) uint16 {
+	_ = b[1]
 	return uint16(b[0]) | uint16(b[1])<<8
 }
 
 func (Native) PutUint16(b []byte, v uint16) {
+	_ = b[1]
 	b[0] = byte(v)
 	b[1] = byte(v >> 8)
 }
 
 func (Native) Uint32(b []byte) uint32 {
+	_ = b[3]
 	return uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
 }
 
 func (Native) PutUint32(b []byte, v uint32) {
+	_ = b[3]
 	b[0] = byte(v)
 	b[1] = byte(v >> 8)
 	b[2] = byte(v >> 16)
@@ -27,11 +31,13 @@ func (Native) PutUint32(b []byte, v uint32) {
 }
 
 func (Native) Uint64(b []byte) uint64 {
+	_ = b[7]
 	return uint64(b[0]) | uint64(b[1])<<8 | uint64(b[2])<<16 | uint64(b[3])<<24 |
 		uint64(b[4])<<32 | uint64(b[5])<<40 | uint64(b[6])<<48 | uint64(b[7])<<56
 }
 
 func (Native) PutUint64(b []byte, v uint64) {
+	_ = b[7]
 	b[0] = byte(v)
 	b[1] = byte(v >> 8)
 	b[2] = byte(v >> 16)
